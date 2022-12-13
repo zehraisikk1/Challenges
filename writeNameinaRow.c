3e1f4ad4002b0e19c698b7aea4e3b88a33121aa4
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-13 10:15:26 +0300

    Create writeNameinaRow.c

---
+++ writeNameinaRow.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main(){
+ int i,j,k,n; 
+ printf("Enter the number of the row :");
+ scanf("%d",&n);
+ 
+ for(i=0;i<n;i++){
+     for(j=0;i<n;i++){
+     
+     
+ }
+     printf("Akif")
+ }
+ 
+    
+}
