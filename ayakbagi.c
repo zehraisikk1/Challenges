5f6755fb693f87de8b8f96695e615e866798a340
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-13 19:26:07 +0300

    Rbt

--- ayakbagi.c
+++ ayakbagi.c
@@ -2,3 +2,18 @@
 
 //Bir döngü kullanarak şu cümleyi yazdırın.
 // cümle : Başarının önündeki bütün ayak bağlarından kurtulmalısın.  Bütün akrabaya, eşe ve dosta hoşçakal demeyi göze almalısın. 'Your only limit is you.'
+
+
+#include <stdio.h>
+
+int main() {
+    char sentence[500];
+    printf("Enter a sentence: ");
+    scanf("%[^\n]", sentence);
+    printf("You entered: %s", sentence);
+    return 0;
+
+}
+
+// output= Başarının önündeki bütün ayak bağlarından kurtulmalısın.  Bütün akrabaya, eşe ve dosta hoşçakal demeyi göze almalısın. 'Your only limit is you.'
+
