186c247554fe1d02f6191b74a016d8a0dc15b9a9
Zehra Işık <74448702+zehraisikk1@users.noreply.github.com>
2022-12-13 10:07:05 +0300

    Zehra Işık

--- printYourName.c
+++ printYourName.c
@@ -1,1 +1,20 @@
 // your name surname and age from user
+int main() {
+	
+	
+	char name[10];
+	char surname[10];
+	int age;
+	printf("enter the name :");
+	scanf("%s",name);
+	
+	printf("enter the surname :");
+	scanf("%s",surname);
+	
+	printf("enter the age :");
+	scanf("%d",&age);
+  
+  printf("ur name "%s" ur surname "%s" ur age "%d",name,surname,age:");
+  
+	return 0;
+}
