05b5acf6cdfb80bc0548ee49ebac6429b7c329bb
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-13 15:14:16 +0300

    Create ayakbagi.c

---
+++ ayakbagi.c
@@ -0,0 +1,4 @@
+//Print a lengthy statement using a loop.
+
+//Bir döngü kullanarak şu cümleyi yazdırın.
+// cümle : Başarının önündeki bütün ayak bağlarından kurtulmalısın.  Bütün akrabaya, eşe ve dosta hoşçakal demeyi göze almalısın. 'Your only limit is you.'
