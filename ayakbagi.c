26062390f14862d31f3d1d6b3bd799fa624f8be6
AdBuran <116068005+AdBuran@users.noreply.github.com>
2022-12-14 11:57:39 +0300

    AdemBuranayakbagi.c

--- ayakbagi.c
+++ ayakbagi.c
@@ -2,15 +2,18 @@
 
 //Bir döngü kullanarak şu cümleyi yazdırın.
 // cümle : Başarının önündeki bütün ayak bağlarından kurtulmalısın.  Bütün akrabaya, eşe ve dosta hoşçakal demeyi göze almalısın. 'Your only limit is you.'
-
-
-#include <stdio.h>
-
-int main() {
-    char sentence[500];
+//scanf'e girdi olarak 500 karakter sınırlı bir cümle girelebilir
+#include "stdio.h"
+#include "stdlib.h"
+int main()
+{
+char sentence[500];
+    do{
     printf("Enter a sentence: ");
-    scanf("%[^\n]", sentence);
+    scanf("%[^\n]%*c",sentence);
     printf("You entered: %s", sentence);
+    }
+    while(0);
     return 0;
 
 }
